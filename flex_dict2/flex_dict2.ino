7ef839b083ced265490d2255f7ceb1483983235c
Annabel Consilvio <annabelconsilvio@annabels-mbp.olin.edu>
2015-11-03 11:10:39 -0500

    code fo flex sensors working

--- flex_dict2/flex_dict2.ino
+++ flex_dict2/flex_dict2.ino
@@ -1,12 +1,16 @@
-const int flexpin = 0; 
-int Matrix[3][5] = { {770 ,770,770,770, 770},
-                    {800,800,800,800, 800},
-                    {850,850,850,850, 850}
+const int flexpin1 = 0; 
+const int flexpin2 = 2; 
+const int flexpin3 = 3; 
+const int flexpin4 = 4; 
+const int flexpin5 = 5; 
+int Matrix[3][5] = { {777 ,777,777,777, 777},
+                    {820,820,820,820, 820},
+                    {860,860,860,860, 860}
                      };
 
 //int flexposition[] =  {600, 600, 600, 600, 600};
-int tolerance = 5;   
-int long flex;
+int tolerance = 5;    
+long unsigned flex;
 
                 
 void setup() {
@@ -18,13 +22,18 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
-  flex = analogRead(flexpin);
-  int flexposition[] = {flex, flex, flex, flex, flex};
+ 
   for (int i =0; i < 3; i++) {  //3 is size of matrix initalized above
     for (int j =0; j < 5; j++) {
+       flex1 = analogRead(flexpin1);
+       flex2 = analogRead(flexpin2);
+       flex3 = analogRead(flexpin3);
+       flex4 = analogRead(flexpin4);
+       flex5 = analogRead(flexpin5);
+  int flexposition[] = {flex1, flex2, flex3, flex4, flex5};
       if ((flexposition[j] <= Matrix[i][j] + tolerance)  && (flexposition[j] >= Matrix[i][j] - tolerance) ){
         if ( j == 4){
-          if( Matrix[i][j] == 770){
+          if( Matrix[i][j] == 777){
              Serial.print("open");
              Serial.print(" ");
              Serial.println(flexposition[1]);
@@ -32,7 +41,7 @@ void loop() {
              j = 0;
              break;
             }
-          else if( Matrix[i][j] == 800){
+          else if( Matrix[i][j] == 820){
              Serial.print("bent");
              Serial.print(" ");
              Serial.println(flexposition[1]);
@@ -41,7 +50,7 @@ void loop() {
              break;
             }
 
-          else if ( Matrix[i][j] == 850){
+          else if ( Matrix[i][j] == 860){
              Serial.print("closed");
              Serial.print(" ");
              Serial.println(flexposition[1]);
@@ -51,7 +60,7 @@ void loop() {
             }
             break;
           }
-          
+                     
         }
       else{
         j = 0;
